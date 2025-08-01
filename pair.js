184afbbc3bda4ee6249a97804e8848cc223fec33
Anonymous dev <kibuukauthuman0@gmail.com>
2025-08-01 11:34:02 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -1,100 +1,87 @@
-const PastebinAPI = require('pastebin-js'),
-pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
-const {makeid} = require('./id');
+const PastebinAPI = require('pastebin-js');
+const pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL');
+const { makeid } = require('./id');
 const express = require('express');
 const fs = require('fs');
-let router = express.Router()
-const pino = require("pino");
+let router = express.Router();
+const pino = require('pino');
 const {
     default: Mbuvi_Tech,
     useMultiFileAuthState,
     delay,
     makeCacheableSignalKeyStore,
     Browsers
-} = require("maher-zubair-baileys");
+} = require('@whiskeysockets/baileys');
 
-function removeFile(FilePath){
-    if(!fs.existsSync(FilePath)) return false;
-    fs.rmSync(FilePath, { recursive: true, force: true })
- };
+function removeFile(FilePath) {
+    if (!fs.existsSync(FilePath)) return false;
+    fs.rmSync(FilePath, { recursive: true, force: true });
+}
+
 router.get('/', async (req, res) => {
     const id = makeid();
     let num = req.query.number;
-        async function MBUVI_MD_PAIR_CODE() {
-        const {
-            state,
-            saveCreds
-        } = await useMultiFileAuthState('./temp/'+id)
-     try {
+    
+    async function Mbuvi_MD_PAIR_CODE() {
+        const { state, saveCreds } = await useMultiFileAuthState('./temp/' + id);
+        try {
             let Pair_Code_By_Mbuvi_Tech = Mbuvi_Tech({
                 auth: {
                     creds: state.creds,
-                    keys: makeCacheableSignalKeyStore(state.keys, pino({level: "fatal"}).child({level: "fatal"})),
+                    keys: makeCacheableSignalKeyStore(state.keys, pino({ level: 'fatal' }).child({ level: 'fatal' })),
                 },
                 printQRInTerminal: false,
-                logger: pino({level: "fatal"}).child({level: "fatal"}),
-                browser: ["Chrome (Ubuntu)", "Chrome (Linux)", "Chrome (MacOs)"]
-             });
-             if(!Pair_Code_By_Mbuvi_Tech.authState.creds.registered) {
+                logger: pino({ level: 'fatal' }).child({ level: 'fatal' }),
+                browser: Browsers.macOS('Chrome')
+            });
+
+            if (!Pair_Code_By_Mbuvi_Tech.authState.creds.registered) {
                 await delay(1500);
-                        num = num.replace(/[^0-9]/g,'');
-                            const code = await Pair_Code_By_Mbuvi_Tech.requestPairingCode(num)
-                 if(!res.headersSent){
-                 await res.send({code});
-                     }
-                 }
-            Pair_Code_By_Mbuvi_Tech.ev.on('creds.update', saveCreds)
-            Pair_Code_By_Mbuvi_Tech.ev.on("connection.update", async (s) => {
-                const {
-                    connection,
-                    lastDisconnect
-                } = s;
-                if (connection == "open") {
-                await delay(5000);
-                let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
-                await delay(800);
-               let b64data = Buffer.from(data).toString('base64');
-               let session = await Pair_Code_By_Mbuvi_Tech.sendMessage(Pair_Code_By_Mbuvi_Tech.user.id, { text: 'Anonymous~' + b64data });
+                num = num.replace(/[^0-9]/g, '');
+                const code = await Pair_Code_By_Mbuvi_Tech.requestPairingCode(num);
+                if (!res.headersSent) {
+                    await res.send({ code });
+                }
+            }
 
-               let MBUVI_MD_TEXT = `
-┏━━━━━━━━━━━━━━
-┃ANONYMOUS-XMD SESSION IS 
-┃SUCCESSFULLY
-┃CONNECTED 💯🔥
-┗━━━━━━━━━━━━━━━
-▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-❶ || Creator =ANONYMOUS POWER 
-      Designed=KING Orman
-▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-❷ || WhattsApp Channel = https://whatsapp.com/channel/0029Vb57ZHh7IUYcNttXEB3y
-▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-❸ || Owner = https://wa.me/+254784937112
-▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-❺ || GIT HUB REPO = https://github.com/Terrizev/ANONYMOUS-XMD/fork
-▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+            Pair_Code_By_Mbuvi_Tech.ev.on('creds.update', saveCreds);
+            Pair_Code_By_Mbuvi_Tech.ev.on('connection.update', async (s) => {
+                const { connection, lastDisconnect } = s;
+                if (connection === 'open') {
+                    await delay(5000);
+                    let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+                    await delay(800);
+                    let b64data = Buffer.from(data).toString('base64');
+                    let session = await Pair_Code_By_Mbuvi_Tech.sendMessage(Pair_Code_By_Mbuvi_Tech.user.id, { text: 'Anonymous~' + b64data });
 
-> Cʀᴇᴀᴛᴇᴅ ʙʏ ᴍʀ Uthuman Dev😌`
+                    let Mbuvi_MD_TEXT = `
+        
+╔════════════════════◇
+║『 SESSION CONNECTED』
+║ ✨ANONYMOUS-MD👻
+║ ✨Terrivez🤓
+╚════════════════════╝`;
 
- await Pair_Code_By_Mbuvi_Tech.sendMessage(Pair_Code_By_Mbuvi_Tech.user.id,{text:MBUVI_MD_TEXT},{quoted:session})
- 
+                    await Pair_Code_By_Mbuvi_Tech.sendMessage(Pair_Code_By_Mbuvi_Tech.user.id, { text: Toxic_MD_TEXT }, { quoted: session });
 
-        await delay(100);
-        await Pair_Code_By_Mbuvi_Tech.ws.close();
-        await removeFile('./temp/'+id);
-   require('child_process').exec('pm2 restart PAIR');
-            } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+                    await delay(100);
+                    await Pair_Code_By_Mbuvi_Tech.ws.close();
+                    return await removeFile('./temp/' + id);
+                } else if (connection === 'close' && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
                     await delay(10000);
-                    MBUVI_MD_PAIR_CODE();
+                    Mbuvi_MD_PAIR_CODE();
                 }
             });
         } catch (err) {
-            console.log("service restated");
-            await removeFile('./temp/'+id);
-         if(!res.headersSent){
-            await res.send({code:"Service Currently Unavailable"});
-         }
+            console.log('Service restarted');
+            await removeFile('./temp/' + id);
+            if (!res.headersSent) {
+                await res.send({ code: 'Service Currently Unavailable' });
+            }
         }
     }
-    return await MBUVI_MD_PAIR_CODE()
+    
+    return await Mbuvi_MD_PAIR_CODE();
 });
-module.exports = router
+
+module.exports = router;
