f4b3d0b981272414e594e29e35a779d68c8e2505
Anonymous dev <kibuukauthuman0@gmail.com>
2025-08-01 12:12:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,19 +3,29 @@ const app = express();
 __path = process.cwd()
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
-    code = require('./pair');
+const code = require('./pair');
 require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
+
+// Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
+app.get('/', (req, res) => {
+    res.sendFile(__path + '/pair.html');
+});
+
+app.use('/code', code);
+app.use('/pair', (req, res) => {
+    res.sendFile(__path + '/pair.html');
+});
+
+// Start server
 app.listen(PORT, () => {
     console.log(`
 Don't Forget To Give Star
 
- Server running on http://localhost:` + PORT)
-})
+ Server running on http://localhost:${PORT}`)
+});
 
-module.exports = app
+module.exports = app;
